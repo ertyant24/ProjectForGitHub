6b82e04470c239d808ac92eb53513138502db363
ertyant24 <erikciertugrul@gmail.com>
2023-01-05 00:42:28 +0300

    subtraction added.

--- ProjectForGitHub/Program.cs
+++ ProjectForGitHub/Program.cs
@@ -2,12 +2,18 @@
 {
     private static void Main(string[] args)
     {
-        int a = 5; 
+        int a = 5;
         int b = 6;
         int c = 7;
 
         int toplam = a + b + c;
         string _msg = $"Toplam : {toplam}";
         Console.WriteLine(_msg);
+        
+        Console.WriteLine("");
+
+        int cıkarma = (a + b) - c;
+        string _msg1 = $"Çıkarma : {cıkarma}";
+        Console.WriteLine(_msg1);
     }
 }
